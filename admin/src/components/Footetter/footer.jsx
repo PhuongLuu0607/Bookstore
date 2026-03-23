899adf97b810b2a3a3efa82bcfc8ae872434b56b
Thi Ha Phuong Luu <PhuongLuu@my.unt.edu>
2026-03-23 03:49:41 -0500

    Add Footer component to the project

---
+++ admin/src/components/Footetter/footer.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+export default function Footer() {
+  return (
+
+    <Fragment>
+            <footer className="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                    <span>Edit &copy; By BOOK HUB 2025</span>
+                    </div>
+                </div>
+            </footer>
+
+    </Fragment>
+
+  )
+}
